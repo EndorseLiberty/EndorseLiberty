f0fefc7e5a322aa85e6e8e5bb9c81117c36b4a26
Jonathan Geggatt <jonathan@adealio.com>
2012-01-16 14:13:40 -0600

    why wont this work

--- config/compass.rb
+++
@@ -1,7 +0,0 @@
-require 'html5-boilerplate'
-
-project_type = :rails
-preferred_syntax = :sass
-sass_dir = "app/assets/stylesheets"
-css_dir = "app/assets/stylesheets/compiled"
-images_dir = "app/assets/images"
