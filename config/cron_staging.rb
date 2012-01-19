3d2df4ff9e5f56151909af58099afb373dcc7af4
Jonathan Geggatt <jonathan@adealio.com>
2012-01-19 12:19:44 -0600

    specify staging env in staging crontab

--- config/cron_staging.rb
+++ config/cron_staging.rb
@@ -1,7 +1,7 @@
 appname = "endorseliberty"
-job_type :lockrun_scheduled_task, "/usr/bin/lockrun --lockfile=/tmp/:task.lockrun -- sh -c \"cd /data/#{appname}/current && RAILS_ENV=:environment bundle exec rake scheduled_tasks::task --silent :output\""
-job_type :lockrun_rake, "/usr/bin/lockrun --lockfile=/tmp/:task.lockrun -- sh -c \"cd /data/#{appname}/current && RAILS_ENV=:environment bundle exec rake :task --silent :output\""
-job_type :lockrun_runner, "/usr/bin/lockrun --lockfile=/tmp/:task.lockrun -- sh -c \"cd /data/#{appname}/current && script/rails runner -e :environment ':task' :output \""
+job_type :lockrun_scheduled_task, "/usr/bin/lockrun --lockfile=/tmp/:task.lockrun -- sh -c \"cd /data/#{appname}/current && RAILS_ENV=staging bundle exec rake scheduled_tasks::task --silent :output\""
+job_type :lockrun_rake, "/usr/bin/lockrun --lockfile=/tmp/:task.lockrun -- sh -c \"cd /data/#{appname}/current && RAILS_ENV=staging bundle exec rake :task --silent :output\""
+job_type :lockrun_runner, "/usr/bin/lockrun --lockfile=/tmp/:task.lockrun -- sh -c \"cd /data/#{appname}/current && script/rails runner -e staging ':task' :output \""
 
 every 5.minutes do
   lockrun_scheduled_task "send_queued_invites"
